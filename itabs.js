ab730c5433f9564f04a8d502907bc75cb2e60cc0
root <root@wallrio.com>
2016-05-10 13:49:29 -0300

    changed

--- itabs.js
+++ itabs.js
@@ -1,5 +1,5 @@
 /**
- * iTabs v1.1 - 27/04/2016
+ * iTabs v1.2 - 10/05/2016
  * divide of content by tab
  *
  * developed by Wallace Rio <wallrio@gmail.com>
@@ -16,10 +16,6 @@
 	var slideNow = null;
 	var countITabs = 0;
 
-/**
- * fix compatible getElementsByClassName to ie8 
- */
-if (!document.getElementsByClassName) {var indexOf = [].indexOf || function(prop) {for (var i = 0; i < this.length; i++) {if (this[i] === prop) return i; } return -1; }; getElementsByClassName = function(className, context) {var elems = document.querySelectorAll ? context.querySelectorAll("." + className) : (function() {var all = context.getElementsByTagName("*"), elements = [], i = 0; for (; i < all.length; i++) {if (all[i].className && (" " + all[i].className + " ").indexOf(" " + className + " ") > -1 && indexOf.call(elements, all[i]) === -1) elements.push(all[i]); } return elements; })(); return elems; }; document.getElementsByClassName = function(className) {return getElementsByClassName(className, document); }; if(Element) {Element.prototype.getElementsByClassName = function(className) {return getElementsByClassName(className, this); }; } }
 
 	var preFunctions = function(idTab){
 		TabNow = idTab || null;
